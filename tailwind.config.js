959cb2533edd8c80c8be4f2253daac5246d48fcc
Luiz Bernardi <luizbernardi37@gmail.com>
2024-08-29 20:07:29 -0300

    feat(projetoEsqueleto)

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        redlight: '#E55959',
+        greenlight: '#7FCDCD',
+        graylight: '#E7E7E7',
+        graytext: '#D9D9D9',
+        redbackground: '#EED0D0',
+        orangetext: '#C25500',
+        tela1white: '#ffffff',
+      }
+    }
+  },
+  plugins: [],
+}
+
